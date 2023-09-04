9e91c007dcf68990c634a2b1ff8525e220d4c8e8
Anton <anton6733@gmail.com>
2023-09-04 23:08:46 +0300

    timeot

---
+++ sem2/timeout_1.py
@@ -0,0 +1,19 @@
+"""
+✔ Создайте вручную список с повторяющимися целыми числами.++
+✔ Сформируйте список с порядковыми номерами
+нечётных элементов исходного списка.
+✔ Нумерация начинается с единицы.
+"""
+
+# не то
+import random
+# from random import choice
+#
+#
+# def main(list_size: int):
+#     temp = [i for i in [choice((range(10))) for i in range(list_size)] if i % 2 != 0]
+#     for item in enumerate(temp, start=1):
+#         print(f"{item[0]}.{item[1]}")
+#
+#
+# main(30)
