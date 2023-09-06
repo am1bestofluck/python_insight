0851ff43065b9d64818573b3e6b81baa39a5d5cd
Anton <optimavar310am@gmail.com>
2023-09-06 10:46:31 +0300

    timeout2, sem3

---
+++ dz3/timeout_2_sml.py
@@ -0,0 +1,1 @@
+../sem3/timeout_2.py
--- sem3/timeout_2.py
+++ sem3/timeout_2.py
@@ -1,23 +1,22 @@
 """
 ✔ Три друга взяли вещи в поход. Сформируйте
 словарь, где ключ — имя друга, а значение —
-кортеж вещей. Ответьте на вопросы:
-✔ Какие вещи взяли все три друга
-✔ Какие вещи уникальны, есть только у одного друга
+кортеж вещей. Ответьте на вопросы:+
+✔ Какие вещи взяли все три друга+
+✔ Какие вещи уникальны, есть только у одного друга+
 ✔ Какие вещи есть у всех друзей кроме одного
-и имя того, у кого данная вещь отсутствует
+и имя того, у кого данная вещь отсутствует+
 ✔ Для решения используйте операции
 с множествами. Код должен расширяться
-на любое большее количество друзей.
+на любое большее количество друзей.+
 """
-from random import sample
-from string import printable
 from pprint import PrettyPrinter
+from random import sample
 
 main_out = PrettyPrinter(compact=True)
 
 
-class Container:
+class Container:  # не пригодился класс то =\ =)
     def __init__(self, name: str, items: set[str]):
         self.name = name
         self.items = items
@@ -26,10 +25,13 @@ class Container:
         self.items.add(item)
 
     def __str__(self):
-        return f"Набор вещей {self.name},из {self.items}"
+        return f"Рюкзак {self.name}"
+
+    def __repr__(self):
+        return f"Набор вещей {self.name},из {self.items!r}"
 
 
-def main(*, friends: int, items_pool: int, items_per_friend: int):
+def main(*, friends: int, items_pool: int, items_per_friend: int) -> dict[int, Container | tuple]:
     """
     :param friends: кол-во друзей
     :param items_pool: сколько вообще есть вещей <= 100
@@ -48,7 +50,7 @@ def main(*, friends: int, items_pool: int, items_per_friend: int):
     for i in range(friends):
         out[f"forgotten_{i}"] = set()
     temp_diff = set()
-    temp_shared= set()
+    temp_shared = set()
     buffer_shared = overall_items[0]
     for i in range(friends):
         try:
@@ -58,11 +60,19 @@ def main(*, friends: int, items_pool: int, items_per_friend: int):
 
         out["unique"] = overall_items[i].difference(temp_diff)
         temp_diff = temp_diff.union(overall_items[i])
-        other_friends = list(range(friends)).remove(i)
+        other_friends = list(range(friends))
+        other_friends.remove(i)
+        temp_shared_by_2 = set()
         for j in other_friends:
-            pass
-    return overall_items, out
+            temp_shared_by_2 = overall_items[j] if not temp_shared_by_2 else temp_shared_by_2.intersection(
+                overall_items[j])
+        out[f"forgotten_{i}"] = temp_shared_by_2.difference(overall_items[i])
+    for i in range(friends):
+        out[i] = Container(i, overall_items[i])
+    return out
 
 
 if __name__ == '__main__':
-    print(main_out.pprint(main(friends=3, items_per_friend=25, items_pool=50)))
+    for counter in [3, 5, 2]:
+        print(main_out.pprint(main(friends=counter, items_per_friend=25, items_pool=50)))
+        print("\n" * 3)
