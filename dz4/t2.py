abc2caa57dfb01bfd65c20756c3be521adbfb27c
Anton <anton6733@gmail.com>
2023-09-07 22:01:42 +0300

    dz2_t2

--- dz4/t2.py
+++ dz4/t2.py
@@ -3,3 +3,19 @@
 где ключ — значение переданного аргумента, а значение — имя аргумента.
 Если ключ не хешируем, используйте его строковое представление.
 """
+from pprint import pp
+
+
+def main(**kwargs):
+    out = dict()
+    for item_name in kwargs:
+        try:
+            out[kwargs[item_name]] = item_name
+        except TypeError:
+            out[str(kwargs[item_name])] = item_name
+    return out
+
+
+if __name__ == '__main__':
+    pp(main(b=[], o=set(), r=dict(),
+            i=1, n=1.2, g=False, x=print, d=globals))
