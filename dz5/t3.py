f128e8861de22d8f0d29cdc506428f6cf1089c99
Anton <anton6733@gmail.com>
2023-09-10 21:20:08 +0300

    dz5_t3

--- dz5/t3.py
+++ dz5/t3.py
@@ -2,5 +2,17 @@
 ✔ Создайте функцию генератор чисел Фибоначчи (см. Википедию).
 """
 
+
 def gfib():
-    yield None
+    a, b = 0, 1
+    while True:
+        a, b = b, a + b
+        yield a, b
+
+
+inf = iter(gfib())
+
+while True:
+    print(print(next(inf)))
+    if input("q?").lower() == "q":
+        break
