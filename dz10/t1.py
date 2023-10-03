2b54f4ac6c0f2fc02c4a53ad03627b00b0fa42b9
Anton <anton6733@gmail.com>
2023-10-03 20:33:12 +0300

    dz10 t1

--- dz10/t1.py
+++ dz10/t1.py
@@ -12,21 +12,40 @@ class Pet(Enum):
 
 
 class PetFabric:
+
     def __init__(self):
         self.validNames = [e.name for e in Pet]
+        self.__shared = {'name': None, 'birthyear': None, 'weight': None}
+        self.__unique_traits = {'bird': {'water_type': None},
+                                'fish': {'water_type': None},
+                                'cat': {'fur': None}
+                                }
 
     def newPet(self, *args) -> list[Animal]:
         out = []
         for new in args:
-            if new in self.validNames:
-                out.append(self._gather_info(new))
+            if new.lower() in self.validNames:
+                out.append(self._gather_info(new.lower()))
             else:
                 print(f"bad input: {new}")
         return out
 
     def _gather_info(self, pet_type: Pet) -> Fish | Bird | Cat:
-        print(pet_type)
-        return pet_type
+        for i in self.__shared:
+            self.__shared[i] = input(f"Input {i} of the {pet_type}")
+        for j in self.__unique_traits[pet_type]:
+            self.__unique_traits[pet_type][j] = input(f"Input {j} of the {pet_type}")
+        match pet_type:
+            case "bird":
+                return Bird(birth_year=self.__shared['birthyear'],weight=self.__shared['weight'],
+                            name=self.__shared['name'],
+                            food=self.__unique_traits[pet_type][j])
+            case "cat":
+                return Cat(birth_year=self.__shared['birthyear'],weight=self.__shared['weight'],
+                           fur=self.__unique_traits[pet_type][j],name=self.__shared['name'])
+            case "fish":
+                return Fish(birth_year=self.__shared['birthyear'],weight=self.__shared['weight'],
+                            name=self.__shared['name'],water_type=self.__unique_traits[pet_type][j])
 
 
 if __name__ == '__main__':
