9d3fd6d2f9684aa615771f86a06412403ce0aa85
Anton <optimavar310am@gmail.com>
2023-10-18 13:13:43 +0300

    sem15

--- sem15/t_6.py
+++ sem15/t_6.py
@@ -13,22 +13,52 @@
 
 import argparse
 from collections import namedtuple
+import logging
 from pathlib import Path
+from typing import Callable
 
 
 def arg_parse() -> Path:
     parser = argparse.ArgumentParser()
     parser.add_argument('arg_path', metavar='path', nargs=1)
-    # pdb.set_trace()
     return Path(parser.parse_args().arg_path[0])
 
 
+def log_path(dst: Path):
+    def inner(func: Callable):
+        def wrapper(*args, **kwargs):
+            nonlocal dst
+            logging.basicConfig(filename=dst, filemode="at", encoding="utf-8",
+                                level=logging.INFO)
+            out: namedtuple = func(*args)
+            # pdb.set_trace()
+            logging.info(str(out._asdict()))
+
+        return wrapper
+
+    return inner
+
+
+@log_path(Path("t6_log.log"))
 def describe_obj(path: Path) -> namedtuple:
-    FileSpecs = namedtuple(typename="FileSpecs", field_names="folder_file parent_folder")
-    pdb.set_trace()
-    return  FileSpecs(path.suffix,path.parts)
+    FileSpecs = namedtuple(typename="FileSpecs",
+                           field_names="name  extension is_dir_ parent_folder")
+    # pdb.set_trace()
+    tmp_ = path.absolute()
+    name_ = tmp_.stem if tmp_.is_file() else tmp_.parts[-1]
+    extension_ = tmp_.suffix if tmp_.is_file() else None
+    is_dir__ = tmp_.is_dir()
+    # pdb.set_trace()
+    try:
+        parent_ = tmp_.parts[-2]
+    except IndexError:
+        parent_ = None
+    return FileSpecs(name=name_, extension=extension_, is_dir_=is_dir__,
+                     parent_folder=parent_)
+
 
 if __name__ == '__main__':
     import pdb
+
     tmp = arg_parse()
     describe_obj(tmp)
